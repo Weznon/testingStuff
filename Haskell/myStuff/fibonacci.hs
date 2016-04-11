c655ab41fd803c108a57ffee979a5aa5212e18c2
Wen Plotnick <Plotnw@ads.d-e.org>
2016-04-11 10:41:01 -0400

    Added fibonacci generator

---
+++ Haskell/myStuff/fibonacci.hs
@@ -0,0 +1,10 @@
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+
+main :: IO ()
+main = do
+          putStrLn "Which fibonacci number?"
+          x <- getLine
+          print $ fib (read x :: Int)
