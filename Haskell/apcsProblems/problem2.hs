0c80e69461958651519348735992d0b8a60e07c4
Wen Plotnick <Plotnw@ads.d-e.org>
2016-04-11 18:16:21 -0400

    fixed warnings

--- Haskell/apcsProblems/problem2.hs
+++ Haskell/apcsProblems/problem2.hs
@@ -1,9 +1,10 @@
-countOfElem elem list = length $ filter (\x -> x == elem) list
+countOfElem :: (Eq a) => a -> [a] -> Int
+countOfElem y list = length $ filter (== y) list
 
 main :: IO()
 main = do
           putStrLn "Type the String: "
           a <- getLine
-          putStrLn "Type the String: "
+          putStrLn "Type the Character: "
           b <- getChar
           print $ countOfElem b a
