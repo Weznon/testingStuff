ee16b1e4e3cea146b903aff2f0f6504249e95c2e
Wen Plotnick <Plotnw@ads.d-e.org>
2016-04-11 15:14:53 -0400

    did some shit

--- Haskell/apcsProblems/problem2.hs
+++ Haskell/apcsProblems/problem2.hs
@@ -1,1 +1,9 @@
-howMany = 
+countOfElem elem list = length $ filter (\x -> x == elem) list
+
+main :: IO()
+main = do
+          putStrLn "Type the String: "
+          a <- getLine
+          putStrLn "Type the String: "
+          b <- getChar
+          print $ countOfElem b a
