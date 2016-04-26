e5c9017dfc432e264d4224c8f89deef83faeebfb
Wen <weznon@localhost.localdomain>
2016-04-25 21:20:47 -0400

    Worked on conwaysRecursion.hs

--- Haskell/myStuff/conwaysRecursion.hs
+++ Haskell/myStuff/conwaysRecursion.hs
@@ -1,3 +1,4 @@
+grid :: [Integer]
 grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -8,6 +9,7 @@ grid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+printGrid :: (Show t) => [t] -> IO ()
 printGrid list = do
                     print [list !! x | x <-[0..9]]
                     print [list !! x | x <-[10..19]]
@@ -20,14 +22,17 @@ printGrid list = do
                     print [list !! x | x <-[80..89]]
                     print [list !! x | x <-[90..99]]
 
-coordGet list x y = if x /= 0 || x /= 9 || y /=0 || y /= 9
-                    then list !! (10 * y + x)
-                    else 0
-getSurround list x y = sum [coordGet list (x - w) (y - z) | w <- [-1..1], z <- [-1..1]] - coordGet list x y
+coordGet :: (Num a) => [a] -> Int -> Int -> a
+coordGet board x y = if x < 0 || x > 9 || y < 0 || y > 9
+                    then 0
+                    else board !! (10 * y + x)
+getSurround :: (Num a) => [a] -> Int -> Int -> a
+getSurround board x y = sum [coordGet board (x - w) (y - z) | w <- [-1..1], z <- [-1..1]] - coordGet board x y
 
-liveOrDie list x y = do
-                        let value = coordGet list x y
-                        let liveNextTo = getSurround list x y
+liveOrDie :: (Num a1, Eq a1, Num a) => [a1] -> Int -> Int -> a
+liveOrDie board x y = do
+                        let value = coordGet board x y
+                        let liveNextTo = getSurround board x y
                         if value == 0
                           then if liveNextTo == 3
                                   then 1
@@ -35,6 +40,13 @@ liveOrDie list x y = do
                         else if liveNextTo == 2 || liveNextTo == 3
                                   then 1
                                 else 0
+generateNextBoard :: (Eq a, Num a, Num t) => [a] -> [t]
+generateNextBoard board = [liveOrDie board x y | x <- [0..9], y <- [0..9]]
+
+lifeGen :: (Eq a, Num a, Eq t, Num t) => [t] -> a -> [[t]]
+lifeGen board iteration
+    |iteration == 0     = board
+    |otherwise          = generateNextBoard board : lifeGen board (iteration - 1)
 main = do
           let grid2 = [liveOrDie grid x y | x <- [0..9], y <- [0..9]]
           printGrid grid
