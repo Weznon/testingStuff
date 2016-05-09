10b75dc6744851c162f74591f3bd38b770bfe8d5
Wen <weznon@localhost.localdomain>
2016-05-09 16:41:18 -0400

    Compiled hailstone on linux

--- Haskell/myStuff/hailstoneSqeuence/hailstone.hs
+++ Haskell/myStuff/hailstoneSqeuence/hailstone.hs
@@ -4,6 +4,7 @@ hailstone :: (Integral a) => a -> a
 hailstone x = if x `mod` 2 == 0
               then x `div` 2
               else x * 3 + 1
+
 hailstoneSequence :: (Integral a) => a -> [a]
 hailstoneSequence 1 = [1]
 hailstoneSequence x = x : hailstoneSequence (hailstone x)
