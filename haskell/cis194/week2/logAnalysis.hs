4d713696eb9fe6a48e3378540315a422c44fd0d0
Wen Plotnick <Plotnw@ads.d-e.org>
2016-05-10 18:12:15 -0400

    worked on week2

--- haskell/cis194/week2/logAnalysis.hs
+++ haskell/cis194/week2/logAnalysis.hs
@@ -1,6 +1,16 @@
-{-# OPTIONS_GHC -Wall #-}
+
 module LogAnalysis where
+
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage x = words
+parseMessage x = let x' = words x in
+                 case x' of
+                   ("I":ts:msg)     -> LogMessage Info (read ts) (unwords msg)
+                   ("W":ts:msg)     -> LogMessage Warning (read ts) (unwords msg)
+                   ("E":num:ts:msg) -> LogMessage (Error (read num)) (read ts) (unwords msg)
+                   _                -> Unknown (unwords x')
+
+parse :: String -> [LogMessage]
+parse x = let x' = lines x in
+          [parseMessage (x' !! y) | y <- [0..(length x' - 1)]]
