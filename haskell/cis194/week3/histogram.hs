37165574af7bbec70d42192ae0f02091baf806c1
Wen Plotnick <Plotnw@ads.d-e.org>
2016-05-13 09:17:35 -0400

    made a half-assed histrogram.hs

--- haskell/cis194/week3/histogram.hs
+++ haskell/cis194/week3/histogram.hs
@@ -1,7 +1,8 @@
 import Data.List
 
-
 parseToPrint _ [] = []
-parseToPrint x xs = if x == (head xs)
-                        then "*":(parseToPrint x (tail xs))
-                        else " ":parseToPrint x (tail xs)
+parseToPrint x xs = if x == head xs
+                        then "*" ++parseToPrint x (tail xs)
+                        else parseToPrint x (tail xs)
+
+parseAll xs = unlines i[show x ++ " | " ++ parseToPrint x xs | x <- [0..9]]
