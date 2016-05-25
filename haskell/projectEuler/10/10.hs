45845ae0db25e33102c45348f8edc354ab60eb3c
Wen Plotnick <Plotnw@ads.d-e.org>
2016-05-25 13:25:43 -0400

    recompiled idek why

--- haskell/projectEuler/10/10.hs
+++ haskell/projectEuler/10/10.hs
@@ -5,3 +5,4 @@ primes = 2 : primes'
   where isPrime (p:ps) n = p*p > n || n `rem` p /= 0 && isPrime ps n
         primes' = 3 : filter (isPrime primes') [5, 7 ..]
 --doesn't stop, but generates the prime pretty damn quickly so...
+--Takes .349 seoncds on the lenovo
