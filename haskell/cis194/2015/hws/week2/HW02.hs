006ec40c16f5fc74e5f1c478cf3dc0cc29b58da0
Wen <weznon@localhost.localdomain>
2016-05-31 13:29:16 -0400

    added shit, worked on cis194)

--- haskell/cis194/2015/hws/week2/HW02.hs
+++ haskell/cis194/2015/hws/week2/HW02.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW02 where
-
+import Data.List
 -- Mastermind -----------------------------------------
 
 -- A peg can be one of six colors
@@ -33,27 +33,27 @@ exactMatches x y = length (filter (\(peg1, peg2) -> peg1 == peg2) (zip x y))
 -- Exercise 2 -----------------------------------------
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
-countColors = undefined
+countColors x = [length . filter (== Red) $ x, length . filter (== Green) $ x, length . filter (== Blue) $ x, length . filter (== Yellow) $ x, length . filter (== Orange) $ x, length . filter (== Purple) $ x]
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches x y = sum (map (\(count1, count2) -> if (count1 > count2) then count2 else count1) (zip (countColors x) (countColors y)))
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove x y = Move y (exactMatches x y) (matches x y - exactMatches x y)
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent (Move x y z) w = y == (exactMatches x w) && z == (matches x w - exactMatches x w)
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes x y = filter (\z -> isConsistent x z) y
 
 -- Exercise 6 -----------------------------------------
 
