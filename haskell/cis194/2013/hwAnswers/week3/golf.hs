634a5d803e00564e3df4382bf5b6acc7678596f1
Wen <weznon@localhost.localdomain>
2016-08-18 17:23:56 -0400

    worked on golf

--- haskell/cis194/2013/hwAnswers/week3/golf.hs
+++ haskell/cis194/2013/hwAnswers/week3/golf.hs
@@ -4,7 +4,15 @@ skips x = [[x !! z | z <- [y-1, 2*y-1..(length x)-1]] | y <- [1..(length x)]]
 
 --Excersize 2, Local Maxima
 localMaxima :: [Integer] -> [Integer]
-localMaxima x = filter isMax x
-                where isMax n = (x !! n+1 < x !! n) && (x !! n-1 < x !! n)
-                      isMax 0 = False
-                      isMax ((length x) - 1) = False
+localMaxima (x:y:z:xs) = if (y > x && y > z) then [y] ++ localMaxima ([y] ++ [z] ++ xs) else localMaxima ([y] ++ [z] ++ xs)
+localMaxima x = []
+
+--Excersize 3, Histogram
+--ayy takes a list of integers and produces a list that says how many of each there are
+ayy :: [Integer] -> [Int]
+ayy x = [length (filter (\z -> z == y) x) | y <- [0..9]]
+
+histrogram :: [Integer] -> String
+histrogram x = 
+                where ayyed = ayy x
+                      maxed = max ayyed
