57092477cd48f348cfaab7e37ed3c6abe1149cf3
Wen <weznon@localhost.localdomain>
2016-08-19 17:44:02 -0400

    wtF

--- startpage/root____files/redir.js
+++ startpage/root____files/redir.js
@@ -7,6 +7,7 @@ function redir(input, args)
 {
 	if (input == "4chan" && args == "empty") {var http = "https://www." + input + ".org";}
 	else if (input =="4chan") {var http = "https://www." + input + ".org/" + args;}
+	else if (input =="github") {var http = "https://www.github.com";}	
 	else if (input == "drive") { var http = "https://www." + input + ".google.com";}
 	else if (input == "reddit" && args == "empty") {var http = "https://www." + input + ".com";}
 	else if  (input == "reddit" && args == "pcmr") {var http = "https://www." + input + ".com/r/pcmasterrace";}
