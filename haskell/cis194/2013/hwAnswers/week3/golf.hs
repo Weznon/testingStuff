2886cb3f824848e0b7928f691c36abee03358a7c
Wen <weznon@localhost.localdomain>
2016-08-26 20:45:41 -0400

    finished the histogram thingie

--- haskell/cis194/2013/hwAnswers/week3/golf.hs
+++ haskell/cis194/2013/hwAnswers/week3/golf.hs
@@ -8,11 +8,29 @@ localMaxima (x:y:z:xs) = if (y > x && y > z) then [y] ++ localMaxima ([y] ++ [z]
 localMaxima x = []
 
 --Excersize 3, Histogram
---ayy takes a list of integers and produces a list that says how many of each there are
-ayy :: [Integer] -> [Int]
-ayy x = [length (filter (\z -> z == y) x) | y <- [0..9]]
+--ayy takes a lsomist of integers and produces a list that says how many of each there are
 
-histrogram :: [Integer] -> String
-histrogram x = 
-                where ayyed = ayy x
-                      maxed = max ayyed
+histogram :: [Int] -> String
+histogram x = (stringer (map parse (binaryifier maxed quan))) ++ "==========\n0123456789"
+                where quan = quantity x
+                      maxed = maximum quan
+
+quantity :: [Int] -> [Int]
+quantity x = [length (filter (\z -> z == y) x) | y <- [0..9]]
+
+binaryifierHelp :: Int -> Int -> Int
+binaryifierHelp x y = if x <= y then 1 else 0
+
+binaryifier :: Int -> [Int] -> [[Int]]
+binaryifier 0 x = []
+binaryifier max' x = map (binaryifierHelp max') x : binaryifier (max'-1) x
+
+
+parse :: [Int] -> String
+parse (0:xs) = " " ++ parse xs
+parse (1:xs) = "*" ++ parse xs
+parse x = ""
+
+stringer :: [String] -> String
+stringer (x:xs) = x ++ "\n" ++ stringer xs
+stringer x = ""
