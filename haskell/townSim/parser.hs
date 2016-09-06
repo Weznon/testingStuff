19be743aab9faffdc2808fadb56ab729ea6ab3ff
Wen Plotnick <Plotnw@ads.d-e.org>
2016-09-06 08:34:38 -0400

    asd

--- haskell/townSim/parser.hs
+++ haskell/townSim/parser.hs
@@ -2,6 +2,22 @@ module Parser where
 
 import Defines
 
-parsePerson :: String -> Person
+--Like words, but with commas
+breaker :: String -> [String]
+breaker [] = []
+breaker xxs@(x:xs)
+        |x == ',' = breaker xs
+        |otherwise = ys : breaker rest
+                where (ys, rest) = break (== ',') xxs
 
+--Takes a string of stats, turns it into StatTable
+parseStats :: String -> StatTable
+parseStats inputS = StatTable (read (b !! 0)) (read (b !! 1)) (read (b !! 2)) (read (b !! 3)) (read (b !! 4)) (read (b !! 5))
+                where b = words inputS
+
+{- Takes an Input such as
 "Bobby Tables the God, 18, 5 5 5 5 5 5, Wounded1"
+and turns it into a Person data
+-}
+parsePerson :: String -> Person
+parsePerson inputS = Person
