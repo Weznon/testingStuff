7d0bfa64719b7496b2448e375a777141a76bb3b5
Wen Plotnick <Plotnw@ads.d-e.org>
2016-09-06 07:46:20 -0400

    worked on town sim

--- haskell/townSim/definitions.hs
+++ haskell/townSim/definitions.hs
@@ -1,3 +1,5 @@
+module Defines where
+
 --Stats
 type STR = Int
 type INT = Int
@@ -5,6 +7,8 @@ type WIS = Int
 type DEX = Int
 type CON = Int
 type CHA = Int
+type Age = Int
+type Name = String
 --For easier stat stuff
 data StatTable = StatTable STR INT WIS DEX CON CHA
                 deriving (Show, Eq)
@@ -14,6 +18,13 @@ data Job = Blacksmith
          | Merchant
          | Guard
 --idk need more
+data Status = Wounded1
+            | Wounded2
+            | Wounded3
 
-data Person = Adult StatTable
-            | Child StatTable
+type Statuses = [Status]
+
+data Person = Adult Name Age StatTable Statuses
+            | Child Name Age StatTable Statuses
+
+type Population = [Person]
---
+++ haskell/townSim/parser.hs
@@ -0,0 +1,7 @@
+module Parser where
+
+import Defines
+
+parsePerson :: String -> Person
+
+"Bobby Tables the God, 18, 5 5 5 5 5 5, Wounded1"
