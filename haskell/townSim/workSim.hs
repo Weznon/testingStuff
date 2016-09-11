092ede2c4b5ae79708e55957ee640f717a29adc3
Wen <weznon@localhost.localdomain>
2016-09-11 19:51:58 -0400

    asd

--- haskell/townSim/workSim.hs
+++ haskell/townSim/workSim.hs
@@ -9,10 +9,12 @@ labourTotal (StatTable str int _ _ con _)       = (str + con + int) `div` 3
 
 --Takes a work table, labour, and returns whether or not the item is finished or not
 isComplete :: WorkTable -> Int -> Bool
-isComplete inputWT inputI       = (snd itemToCheck) <= inputI
+isComplete inputWT inputI       = snd itemToCheck <= inputI
                                 where itemToCheck       = head inputWT
 
-
+--Takes a inventory and worktable, and takes the WorkTable and moves the first thing to inventory
+completedMover :: Inventory -> WorkTable -> Inventory
+completedMover inputI inputWT   = fst (head inputWT) : inputI
 
 
 --Changes to Statuses, Inventory, and Work Table
