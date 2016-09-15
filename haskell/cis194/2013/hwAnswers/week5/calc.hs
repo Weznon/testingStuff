84bdbcf44a762750ec33ec9f83909fdea72939ee
Wen Plotnick <Plotnw@ads.d-e.org>
2016-09-15 12:29:59 -0400

    ads

---
+++ haskell/cis194/2013/hwAnswers/week5/calc.hs
@@ -0,0 +1,11 @@
+module Calc where
+import ExprT
+import Parser
+
+eval :: ExprT -> Integer
+eval (Lit a) = a
+eval (Add a b) = (eval a) + (eval b)
+eval (Mul a b) = (eval a) * (eval b)
+
+evalStr :: String -> Maybe Integer
+evalStr x = fmap eval (Parser.parseExp Lit Add Mul x)
