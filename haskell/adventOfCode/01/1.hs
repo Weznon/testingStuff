17d058e3195ca4f48395d57ff34bd4b0018d5eb7
Weznon <waterwasp55555@gmail.com>
2016-12-17 16:54:15 -0500

    got day 1 p1, still need to do p2

--- haskell/adventOfCode/01/1.hs
+++ haskell/adventOfCode/01/1.hs
@@ -35,8 +35,29 @@ parseInstruction ('R':x) = (Instruction R (read x))
 parseInstruction ('L':x) = (Instruction L (read x))
 parseInstruction _ = (Instruction R 0)
 
-newPos :: (Int, Int) -> [Instruction] -> (Int, Int)
-newPos (x, y) x:xs =
+newPos :: (Int, Int, Int) -> [Instruction] -> (Int, Int, Int)
+newPos (d, x, y) ([]) = (d, x, y)
+newPos (d, x, y) ((Instruction R z):xs) = newPos (m $ d + 1, x + (fst $ change (m $ d + 1) z), y + (snd $ change (m $ d + 1) z)) xs
+newPos (d, x, y) ((Instruction L z):xs) = newPos (m $ d - 1, x + (fst (change (m $ d - 1) z)), y + (snd (change (m $ d - 1) z))) xs
+
+m :: Int -> Int
+m x = x `mod` 4
+
+answer :: (Int, Int, Int) -> Int
+answer (x, y, z) = y + z
+change :: Int -> Int -> (Int, Int)
+change 0 x = (0, x)
+change 1 x = (x, 0)
+change 2 x = (0, -x)
+change 3 x = (-x, 0)
+change _ _ = (0, 0)
+
+
+combine :: (Int, Int, Int) -> (Int, Int) -> (Int, Int, Int)
+combine (x, y, z) (w, q) = (x, y + w, z + q)
+
+input :: String
+input = "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
 
 main :: IO()
-main = print "hi"
+main = print (answer $ newPos (0, 0, 0) (parseComplete input))
