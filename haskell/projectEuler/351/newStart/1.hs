db2065775a2e1fefcc16e89996795d7b5c2ba99b
Weznon <waterwasp55555@gmail.com>
2017-01-01 23:04:25 -0500

    started work on 351 again, added calender stuff too

--- haskell/projectEuler/351/newStart/1.hs
+++ haskell/projectEuler/351/newStart/1.hs
@@ -6,36 +6,38 @@ instance Show Packed where
   show (Num s) = show s
   show Emp = "_"
 
-concat :: Packed -> Packed -> [Packed]
-
-concat Emp z  = [z]
-concat z Emp  = [z]
-concat x z    = [x, z]
-
+--Takes an Integer and packs it
 insert :: Integer -> Packed
 insert x = (Num x)
 
+--Long list of primes, unpacked
 pr :: [Integer]
 pr = takeWhile (\x -> x < 100000000) primes
 
+--shorter list of primes, unpacked
 pr2 :: [Integer]
 pr2 = takeWhile (\x -> x < 100) primes
 
+--Long list of primes, packed
 prime :: [Packed]
 prime = map insert pr
 
+--shorter list of primes, packed
 prime2 :: [Packed]
 prime2 = map insert pr2
 
+--generates a list of packed that says whether its a prime or not
 generatePrimeList :: Packed -> [Packed]
 generatePrimeList (Num x) = take 100 $ cycle $ [Emp | ayy <- [1..(x-1)]] ++ [(Num x)]
 generatePrimeList Emp = take 100 $ cycle [Emp]
 
+--generates the list of packed lists that need to be flattened into the factorizations
 toFlatten :: [[Packed]]
 toFlatten = map generatePrimeList prime2
 
+--generates the prime factorizations
 flatten :: [[Packed]] -> [[Packed]]
-flatten x =
+flatten (x:xs) =
 
 main :: IO()
 main = print $ sum pr
