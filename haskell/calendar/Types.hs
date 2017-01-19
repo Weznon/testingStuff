a7f70421d75f468df88ebfbe7a2982185c0d9684
Weznon <waterwasp55555@gmail.com>
2017-01-18 22:03:50 -0500

    WTF THE PARSER ACTUALLY WORKS

--- haskell/calendar/Types.hs
+++ haskell/calendar/Types.hs
@@ -1,19 +1,10 @@
 --this module defines the various data types used in the calander program
 module Types where
 
-data Event = Event {startDate :: SDate, endDate :: EDate, summary :: Summary, description :: Description}
+data Event = Event {startDate :: SubEvent, endDate :: SubEvent, summary :: SubEvent, description :: SubEvent}
   deriving (Show, Eq)
 
-data Summary = Summary String
-  deriving (Show, Eq)
-
-data SDate = SDate Date
-  deriving (Show, Eq)
-
-data EDate = EDate Date
-  deriving (Show, Eq)
-
-data Description = Description String
+data SubEvent = Summary String | SDate Date | EDate Date | Desc String | Nulp
   deriving (Show, Eq)
 
 data Date = Date {year :: Int, month :: Int, day :: Int}
