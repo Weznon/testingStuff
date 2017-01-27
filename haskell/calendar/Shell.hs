4756ea9edeb791f19c564886d8c5a70c3e6a4bcd
Weznon <waterwasp55555@gmail.com>
2017-01-27 18:28:59 -0500

    added shell file for doing the bash related stuff

---
+++ haskell/calendar/Shell.hs
@@ -0,0 +1,9 @@
+module Shell where
+
+import Types
+
+--This Module is responsible for getting the list from the url
+--Accomplishes this using bash commands since thats what I'm conformtable with
+--i think it should curl the page into a file(?) and then read from that file
+--htat will make lots of io stuff thoguh
+--f
